f2b4c707d0b073d815393abb0e620f8fe0169c96
unknown <hp4129889@gmail.com>
2022-12-12 10:13:21 +0800

    fix: timeHelper getCurrentWeek switch any timezone to +08:00

--- src/util/time/timeHelper.service.ts
+++ src/util/time/timeHelper.service.ts
@@ -7,10 +7,14 @@ import { TimePeriodDto } from "./timeHelper.dto";
 @Injectable()
 export class TimeHelperService {
 
+    private readonly timeFix = new Date().getTimezoneOffset();
+
     getCurrentWeek(data: Date): TimePeriodDto {
         const now = new Date();
+        const hourFix = 8 * 60 + this.timeFix;
         const startDate = new Date(data.setDate(data.getDate() - data.getDay()));
-        const start = new Date(startDate.setHours(0, 0, 0, 0));
+        const startTime = new Date(startDate.setHours(0, 0, 0, 0)).getTime();
+        const start = new Date(startTime - hourFix * 60 * 1000);
         const endDate = new Date(now.setDate(6 - now.getDay() + now.getDate()));
         const end = new Date(endDate.setHours(23, 59, 59, 999));
         const period = {
